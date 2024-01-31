c8219e6fa1029dba0d641c6dd0a5c3a2e2af3576
PimKalasinmongkol <pimty.pim@gmail.com>
2024-01-31 15:12:49 +0700

    drawer v2

--- src/components/MiniDrawer.js
+++ src/components/MiniDrawer.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
-import InboxOutlined from '@ant-design/icons';
-import { CiInboxIn } from "react-icons/ci";
+import { FaInbox } from "react-icons/fa";
+import { FaCircleUser } from "react-icons/fa6";
+import { BsTable } from "react-icons/bs";
+import { CgInsertBeforeR } from "react-icons/cg";
+import { TiUserAdd } from "react-icons/ti";
+import { IoLogOut } from "react-icons/io5";
+import { IoLockOpen } from "react-icons/io5";
 import '../style.css'; // You can create a separate CSS file for styling
 
 const MiniDrawer = () => {
@@ -17,11 +22,45 @@ const MiniDrawer = () => {
                 <div className='drawerMenu'>
                     <div>เมนู</div>
                     <button onClick={toggleDrawer} >
-                        <CiInboxIn size={40} />
+                        <FaInbox size={40} />
                     </button>
                 </div>
-
-                <p>Drawer Content</p>
+                <div className='drawerList'>
+                    <div>โปรไฟล์</div>
+                    <button onClick={toggleDrawer} >
+                        <FaCircleUser size={40} />
+                    </button>
+                </div>
+                <div className='drawerList'>
+                    <div>ตารางสอน</div>
+                    <button onClick={toggleDrawer} >
+                        <BsTable size={40} />
+                    </button>
+                </div>
+                <div className='drawerList'>
+                    <div>นำเข้าหลักสูตร</div>
+                    <button onClick={toggleDrawer} >
+                        <CgInsertBeforeR size={40} />
+                    </button>
+                </div>
+                <div className='drawerList'>
+                    <div>ข้อมูลผู้ใช้งาน</div>
+                    <button onClick={toggleDrawer} >
+                        <TiUserAdd size={40} />
+                    </button>
+                </div>
+                <div className='drawerList'>
+                <div className='bg-lime-600 p-1 rounded-md'>ออกจากระบบ</div>
+                    <button onClick={toggleDrawer} >
+                        <IoLockOpen size={40} />
+                    </button>
+                </div>
+                <div className='drawerList'>
+                    <div>ออกจากระบบ</div>
+                    <button onClick={toggleDrawer} >
+                        <IoLogOut size={40} />
+                    </button>
+                </div>
             </div>
         </div>
     );

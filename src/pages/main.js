ed341a0ed5e57faefab8559e6248720e03b32d35
PimKalasinmongkol <pimty.pim@gmail.com>
2024-01-31 22:53:28 +0700

    version 4

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,12 +1,23 @@
 import '../style.css';
+import { FaCircleUser } from "react-icons/fa6";
 
 const Main = () => {
   return (
-    <div className='flex-row justify-start ml-56'>
-      <div style={{ flex: 20 }} className='mx-5 my-5 flex justify-start text-black text-3xl font-bold'>
-        <p>ยินดีต้อนรับเข้าสู่ระบบ</p>
+    <div className='ml-56'>
+      <div className='mx-5 my-5 flex flex-col justify-start text-black font-bold '>
+        <p className='text-3xl '>ยินดีต้อนรับเข้าสู่ระบบ</p>
+        <div className='flex flex-row p-6 items-center'>
+          <FaCircleUser size={60} />
+          <p className='pl-6 text-rose-color text-2xl '>คุณสมศรี สนุกซุกซน</p>
+        </div>
+        <div className='flex flex-row justify-center'>
+          <div className='bg-from-color rounded-lg  p-5 mx-10 w-9/12 justify-center '>
+              <p className='justify-center text-xl'>ประกาศเพิ่มเติม</p>
+          </div>
+        </div>
       </div>
     </div>
+
   );
 };
 
